eda4f0fe1004218e2d39436a4a64c86aaaf37e17
Dense- <98495564+DenseCode@users.noreply.github.com>
2022-02-10 16:10:43 +0300

    Index.js
    The dm trigger

---
+++ Index.js
@@ -0,0 +1,3 @@
+        if(message.content == 'ping'){     // add replace 'ping' wwith your message/command
+client.users.cache.get(message.author.id).send('pong')  //replace 'pong'  the_message_bot_should_reply_you_in_dms
+    }
